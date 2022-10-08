ea5f9cb98bb2b9b4028e7f86a8dc2e2bae23b339
Scott Willrich <swillrich@gmail.com>
2022-10-08 10:49:16 -0500

    first react component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import Header from './components/Header';
+
 function App() {
+
     return (
-        <h1>Hello from the App Component</h1>
+        <>
+            <Header text="Hello World" />
+            <div className="container">
+                <h1>MyApp</h1>
+            </div>        
+        </>
+
     )
 }
 
