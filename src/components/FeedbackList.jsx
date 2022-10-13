51eaa4ff01880e955edd58ca079e95f665fdc29d
Scott Willrich <swillrich@gmail.com>
2022-10-13 15:45:45 -0500

    added framer-motion to fade in/out feedback items

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 
@@ -8,13 +9,31 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className='feedback-list'>
-        {feedback.map((item) =>(
-            <div key={item.id}>
-                <FeedbackItem item={item} handleDelete={handleDelete} />
-            </div>
-        ))}
+        <AnimatePresence>
+            {feedback.map((item) =>(
+                <div key={item.id}>
+                    <motion.div 
+                        key={item.id}
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        exit={{ opacity: 0}}
+                    >
+                        <FeedbackItem item={item} handleDelete={handleDelete} />
+                    </motion.div>
+                </div>
+            ))}
+        </AnimatePresence>
     </div>
   )
+
+//     <div className='feedback-list'>
+//         {feedback.map((item) =>(
+//             <div key={item.id}>
+//                 <FeedbackItem item={item} handleDelete={handleDelete} />
+//             </div>
+//         ))}
+//     </div>
+//   )
 }
 
 export default FeedbackList
