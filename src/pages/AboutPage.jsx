d18112412ad8e0f6bdc501e5c28f785c95815650
Scott Willrich <swillrich@gmail.com>
2022-10-15 10:20:23 -0500

    adding Link from react-router-dom

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from '../components/shared/Card';
 
 function AboutPage() {
@@ -6,8 +6,10 @@ function AboutPage() {
     <Card>
         <h1>About this Project</h1>
         <p>This is a React app to leave feedback for a product or service</p>
-        <p>Version 1.0.0</p>
-        <p><a href="/">Back to Home</a></p>
+        <p>Version 1.0.0</p>  
+        <p><Link to={{          // this is the same as doing <Link to='/'>Back to Home</Link>
+            pathname: '',       // you can also add query parameters / properties
+        }}>Back to Home</Link></p>
     </Card>
   )
 }
